48d558bafbc1cd06db6cbd4365ed4c693a6a833a
Vitor Correa <vitor.correa@gruporbs.com.br>
2019-02-18 15:03:26 -0300

    Novas telas

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -5,6 +5,8 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
+
+
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
 	email = StringField('Email',validators=[DataRequired(), Email()])
@@ -50,6 +52,7 @@ class UpdateAccountForm(FlaskForm):
 			if user:
 				raise ValidationError('That email is taken. Please Choose a different one.')
 	
+
 
 class PostForm(FlaskForm):
 	title = StringField('Title', validators=[DataRequired()])
