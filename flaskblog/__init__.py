739a363a58168d13a0b64712727a2f721e7de0cd
Vitor Correa <vitor.correa@gruporbs.com.br>
2019-02-19 16:40:43 -0300

    Configurado sender-email mas ainda não sta enviando

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '528fb425e08ccba8c660dbd6d44eda0c'
@@ -11,6 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = '587'
+app.config['MAIL_USER_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
+mail = Mail(app)
 
 from flaskblog import routes
