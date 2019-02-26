7dc2b0817bb03c2cee32d436ffbaf23179c9ac2c
Vitor Correa <vitor.correa@gruporbs.com.br>
2019-02-26 14:16:12 -0300

    Incluir icones like deslike e comment

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -17,8 +17,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = '587'
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USERNAME'] = 'the.turtle.tester@gmail.com'
+app.config['MAIL_PASSWORD'] = 'uniritter'
 mail = Mail(app)
 
 from flaskblog import routes
